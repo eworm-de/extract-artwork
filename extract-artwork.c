eworm-de/extract-artwork

commit 68576d8903da21d0ae4e6f00ca3f71dd98f0b320

update for ffmpeg 4.x
The function av_register_all() is deprecated since commit
0694d8702421e7aff1340038559c438b61bb30dd.

extract-artwork.c
@@ -23,7 +23,9 @@ int main(int argc, char **argv) {
 	}
 
 	/* libav */
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 9, 100)
 	av_register_all();
+#endif
 
 	/* only fatal messages from libav */
 	av_log_set_level(AV_LOG_FATAL);

commit 939f69cec2789fc36c03f9c598c030af369452df

fix libav version check
Checking with LIBAVCODEC_VERSION_INT is stupid if we do not include
libavcodec... Let's use LIBAVFORMAT_VERSION_INT instead.
This broke with commit 68576d8903da21d0ae4e6f00ca3f71dd98f0b320.

extract-artwork.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
 	}
 
 	/* libav */
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 9, 100)
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(58, 9, 100)
 	av_register_all();
 #endif
 